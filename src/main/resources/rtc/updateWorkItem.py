50329b937ea75da0031de16cc37d2f008488d7ee
Rob <robvanstone@gmail.com>
2015-06-19 13:28:44 +0100

    Updated with latest task code

--- src/main/resources/rtc/updateWorkItem.py
+++ src/main/resources/rtc/updateWorkItem.py
@@ -19,26 +19,50 @@ rtcURL = rtcServer['url']
 if rtcURL.endswith('/'):
   rtcURL = rtcURL[:len(rtcURL)-1]
 
+# Form the request
 request = HttpRequest(rtcServer, username, password)
 
-# Now we want to perform an update of our work item - specifically to change the workitem status
+# We want to perform an update of our work item - specifically to change the workitem status
 headers = {'Content-type': 'application/x-oslc-cm-change-request+json', 'Accept' : 'application/x-oslc-cm-change-request+json'}
-#context = workItemURL[len(rtcURL):]
-context = "resource/itemName/com.ibm.team.workitem.WorkItem/" + workItemID
+context = "resource/itemName/com.ibm.team.workitem.WorkItem/" + workItemID 
 
+# Get the workitem we have been chosen
 resp = request.get( context , headers=headers)
-print resp.response
+if resp.isSuccessful:
+  print "Found our workitem"
+else:
+  print "Something went wrong"
+  print resp.errorDump()
+  sys.exit(1)
+  
 
-commentJSON = '{ "dc:description": updateComment }'
-context += "/rtc_cm:comments"
-print "Context is " + context
-resp = request.post( context , body=commentJSON, headers=headers)
+# Now update the state of the workitem
+# You can not update the state directly with a body element you have to invoke an RTC action.
+# RTC Actions are part of the workflow defined for the workitems (Defects, Tasks etc)
+# Different workitem types have different workflows and therefore different actions
+# As a consequence depending on your set up, you may not be able to move from your current state to the desired state
+# This is a big TODO in this plugin
+# Determine the available states
+# Determine if you can move from current to desired
+# Throw error if this is not possible (or silently fail)
+# Or progress through the workflow...
+# The comment added here is in fact a description - more work required to add comments to a task
+################
+states = {"Done":"com.ibm.team.workitem.taskWorkflow.action.resolve",
+          "InProgress":"com.ibm.team.workitem.taskWorkflow.action.startWorking"}
 
+commentString = updateComment + " State changed to " + newState
+body = '{ "dc:description" : "' + commentString + '"}'
+
+statusContext = context + "/rtc_cm:comments?_action=" + states[newState]
+print statusContext
+
+resp = request.put( statusContext, body=body ,headers=headers )
 if resp.isSuccessful:
   print resp.response
 else:
-  print "Something went wrong - stone the crows"
+  print "Something went wrong"
   sys.exit(1)
-
+  print resp.errorDump()
 
 
